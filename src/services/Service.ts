3bb54e194e478465c67882ef45df4bca9a2b56d6
juliohf <juliohcf@hotmail.com>
2024-08-20 02:31:48 +0900

    Deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog-pessoal-x3ew.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const cadastrarUsuario = async (
